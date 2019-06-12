b691e018c37a0f871e3a89d1d7cc5ca7c3b5614b
seyed ali <S.alisohofi@gmail.com>
2019-06-12 11:34:19 +0430

    improve showing users

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-Vue.component('room',{
+Vue.component('peopel',{
     template:`
-    <div>
-        <label> تعداد اتاق:</label>
-        <input type="number" name="">
-        <adult></adult>
-    </div> 
+    <div class="childs" id="rooms-1">
+        <div class="adult">
+            <label>بزرگسال</label>
+            <div class="buttons">
+                <button type="button" class="add">+</button>
+                <input type="number" min="0" value="1">
+                <button type="button" class="remove">-</button>
+            </div>
+        </div>
+        <div class="children">
+            <label>خردسال</label>
+            <div class="buttons">
+                <button type="button" class="add">+</button>
+                <input type="number" min="0" class="child-in" id="child-in" value="0">
+                <button type="button" class="remove">-</button>
+            </div>
+        </div>
+        <div class="infants">
+            <label>نوزاد</label>
+            <div class="buttons">
+                <button type="button" class="add">+</button>
+                <input type="number" min="0" id="infant-in" value="0">
+                <button type="button" class="remove">-</button>
+            </div>
+        </div>
+    </div>
     `
 });
-Vue.component('adult',{
+Vue.component('child',{
     template:`
-    <div v-show="this.hasRoom = true">
-        <label>تعداد افراد بزرگسال:</label>
-        <input type="number" >
-        <label>خردسال دارید؟</label>
-        <input type="checkbox">
-    </div> 
+    <div class="child-out">
+        <label>سن خردسال</label>
+        <div class="buttons">
+            <input type="number" min="0">
+        </div>
+    </div>
+    `
+});
+Vue.component('infant',{
+    template:`
+    <div class="child-out">
+        <label>سن نوزاد</label>
+        <div class="buttons">
+            <input type="number" min="0">
+        </div>
+    </div>
     `
 });
 
